ca12657c5110939e2c89c7b1ce06210a7024ff9b
Thomas <ajna@aj.home>
2019-09-30 22:09:34 +0200

    #scripted -  Fixes again..

--- prepare_my_repo.sh
+++ prepare_my_repo.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /usr/bin/env bash
 blih -u thomas.camlong@epitech.eu repository create $1
 blih -u thomas.camlong@epitech.eu repository setacl $1 ramassage-tek r
 blih -u thomas.camlong@epitech.eu repository getacl $1
--- push_that.sh
+++ push_that.sh
@@ -1,3 +1,3 @@
-git add *
+git add -A
 git commit -m "#scripted -  $1"
 git push origin master
