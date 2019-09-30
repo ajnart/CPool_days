5ddeee1058a84ba14e458fe88d8d0d31671603cd
Thomas <ajna@localhost.localdomain>
2019-09-30 14:32:08 +0200

    Prepare_my_repo upload

---
+++ prepare_my_repo.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+blih -u thomas.camlong@epitech.eu repository create $1
+blih -u thomas.camlong@epitech.eu repository setacl $1 ramassage-tek r
+blih -u thomas.camlong@epitech.eu repository getacl $1
